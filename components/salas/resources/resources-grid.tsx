0944d67cd552119154657448ee95f4779f04b574
Santiago <santiago.gallego28@outlook.com>
2026-09-11 10:18:08 -0500

    fix(ui): add missing useEffect import in resources grid

--- components/salas/resources/resources-grid.tsx
+++ components/salas/resources/resources-grid.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { formatDistanceToNow } from "date-fns"
 import { es } from "date-fns/locale"
 import dynamic from "next/dynamic"
